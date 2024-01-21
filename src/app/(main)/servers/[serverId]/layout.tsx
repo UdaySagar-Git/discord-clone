4f50677bc9134cdde30e3acf12465d0ecd456d83
Uday Sagar <udaysagar.mail@gmail.com>
2024-01-21 13:18:51 +0530

    server sidebar header

--- src/app/(main)/servers/[serverId]/layout.tsx
+++ src/app/(main)/servers/[serverId]/layout.tsx
@@ -34,7 +34,7 @@ const ServerLayout = async (
     <div className="h-full">
       <div 
       className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <ServerSidebar />
+        <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">
         {children}
