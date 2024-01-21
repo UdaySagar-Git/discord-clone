bed06fb8e77c07383c956541beaaa489f75f0b9f
Uday Sagar <udaysagar.mail@gmail.com>
2024-01-21 21:42:05 +0530

    create channel modal

--- src/providers/model-provider.tsx
+++ src/providers/model-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CreateChannelModal } from "@/modals/create-channel-modal";
 import { CreateServerModal } from "@/modals/create-server-modal";
 import { EditServerModal } from "@/modals/edit-server-modal";
 import { InviteModal } from "@/modals/invite-modal";
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
       <InviteModal/>
       <EditServerModal/>
       <MembersModal/>
+      <CreateChannelModal/>
     </>
   )
 }
