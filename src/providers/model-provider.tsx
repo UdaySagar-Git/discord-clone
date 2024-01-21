9eb76b21ebad823bb86c0c516997a3926b729272
Uday Sagar <udaysagar.mail@gmail.com>
2024-01-21 21:20:03 +0530

    edit server

--- src/providers/model-provider.tsx
+++ src/providers/model-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CreateServerModal } from "@/modals/create-server-modal";
+import { EditServerModal } from "@/modals/edit-server-modal";
 import { InviteModal } from "@/modals/invite-modal";
 import { useEffect, useState } from "react";
 
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal/>
+      <EditServerModal/>
     </>
   )
 }
